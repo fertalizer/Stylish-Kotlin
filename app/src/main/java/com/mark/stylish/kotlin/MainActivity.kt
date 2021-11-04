d4413fcf0114e6634588827614193155b841e055
fertalizer <fertalizer54321@gmail.com>
2021-11-04 17:57:13 +0800

    Custom Toolbar.

--- app/src/main/java/com/mark/stylish/kotlin/MainActivity.kt
+++ app/src/main/java/com/mark/stylish/kotlin/MainActivity.kt
@@ -2,10 +2,32 @@ package com.mark.stylish.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
+    lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setToolbar()
     }
+
+    // get the height of status bar from system
+    private val statusBarHeight: Int
+        get() {
+            val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
+            return when {
+                resourceId > 0 -> resources.getDimensionPixelSize(resourceId)
+                else -> 0
+            }
+        }
+
+    fun setToolbar() {
+        toolbar = findViewById(R.id.toolbar)
+        toolbar.setPadding(0, statusBarHeight, 0, 0)
+    }
+
+
 }
