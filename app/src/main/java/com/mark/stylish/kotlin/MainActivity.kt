a7493b7170f6590842a9b4d03b5c0ca11e05c416
fertalizer <fertalizer54321@gmail.com>
2021-11-10 18:13:14 +0800

    Add BottomNavigation
    1. BottomNavigation
    2. App Navigatio

--- app/src/main/java/com/mark/stylish/kotlin/MainActivity.kt
+++ app/src/main/java/com/mark/stylish/kotlin/MainActivity.kt
@@ -2,8 +2,10 @@ package com.mark.stylish.kotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.appcompat.widget.Toolbar
+import android.util.Log
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.mark.stylish.kotlin.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,8 +16,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 //        setContentView(R.layout.activity_main)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main) // Replace setContentView
-
-        setToolbar()
+        setupToolbar()
+        setupBottomNavigation()
     }
 
     // get the height of status bar from system
@@ -28,9 +30,34 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
-    fun setToolbar() {
+    fun setupToolbar() {
         binding.toolbar.setPadding(0, statusBarHeight, 0, 0)
     }
 
+    private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener{ item ->
+        when(item.itemId) {
+            R.id.bottom_navigation_home -> {
+                findNavController(R.id.myNavHostFragment).navigate(R.id.homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.bottom_navigation_catalog -> {
+                findNavController(R.id.myNavHostFragment).navigate(R.id.catalogFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.bottom_navigation_cart -> {
+                findNavController(R.id.myNavHostFragment).navigate(R.id.cartFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.bottom_navigation_profile -> {
+                findNavController(R.id.myNavHostFragment).navigate(R.id.profileFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            else -> false
+        }
+    }
+
+    fun setupBottomNavigation() {
+        binding.bottomNav.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
+    }
 
 }
